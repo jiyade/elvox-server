200b160cd55e63dc9b300bba1af57e838514a286
Jiyad <mdjiyade@gmail.com>
2026-01-23 18:33:12 +0530

    Updated CustomError to include error code

--- utils/CustomError.js
+++ utils/CustomError.js
@@ -1,6 +1,7 @@
 export default class CustomError extends Error {
-    constructor(message, status = 400) {
+    constructor(message, status = 400, code = null) {
         super(message)
         this.status = status
+        this.code = code
     }
 }
